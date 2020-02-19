543f65cab878086ea908ae8fedcee7a4dc208422
omi <omer257@gmail.com>
2020-02-19 16:45:27 +0200

    Updates

---
+++ precache-manifest.f61041829f9697d7d7d4613fbb936475.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57af7dbe62f559e62336af7e11a42b0",
+    "url": "/AnimationNapi/index.html"
+  },
+  {
+    "revision": "1099074c08daa01f127d",
+    "url": "/AnimationNapi/static/css/main.0f593d71.chunk.css"
+  },
+  {
+    "revision": "6026430f122b448bfd92",
+    "url": "/AnimationNapi/static/js/2.76cdb09e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/AnimationNapi/static/js/2.76cdb09e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1099074c08daa01f127d",
+    "url": "/AnimationNapi/static/js/main.a5ba4b9c.chunk.js"
+  },
+  {
+    "revision": "79c1da7cc1fab25b7fda",
+    "url": "/AnimationNapi/static/js/runtime-main.cf3cbd20.js"
+  }
+]);
